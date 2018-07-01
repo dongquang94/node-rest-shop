d55de98f401ef419ba3d812abf119b266bc31543
dvquang <wangdong1994.uneti@gmail.com>
2018-07-01 22:36:24 +0700

    update mongodb validation

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,10 +6,23 @@ const Product = require('../../models/product');
 
 router.get('/', (req, res, next) => {
     Product.find()
+    .select('name price _id')
     .exec()
     .then(docs => {
-        console.log(docs);
-        res.status(200).json(docs);
+        const response = {
+            count : docs.length,
+            products : docs.map(doc => {
+                return {
+                    name : doc.name,
+                    price : doc.price,
+                    _id : doc._id,
+                    request : {
+                        type : 'GET'
+                    }
+                }
+            })
+        };
+        res.status(200).json(response);
     })
     .catch(err => {
         console.log(err);
