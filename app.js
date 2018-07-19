01e5a10b72d76e165b776fda30fc44b3354ffbab
dvquang <wangdong1994.uneti@gmail.com>
2018-07-19 17:30:31 +0700

    added signup api

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect("mongodb+srv://dvquang:9MjYShWBQa4hb2Ty@node-rest-shop-9mnvp.mongodb.net/test?retryWrites=true");
 mongoose.Promise = global.Promise;
@@ -31,6 +32,7 @@ app.use((req, res, next) => {
 // Routes which should handle request
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user/', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
